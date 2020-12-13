270de2baacc3200c060bbc8b8c15449b367c479f
Vladi <vladislav.budnikov@gmail.com>
2020-12-13 22:33:19 +0200

    Adding View Models

---
+++ NeatFleetManagement.Presentation/Models/CarViewModel.cs
@@ -0,0 +1,18 @@
+﻿using NeatFleetManager.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NeatFleetManager.Web
+{
+    public class CarViewModel
+    {
+        public int CarId { get; set; }
+        public CarColor Color { get; set; }
+        public CarCondition Condition { get; set; }
+        [Range(100, 2000000)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ NeatFleetManagement.Presentation/Models/InventoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeatFleetManager.Web
+{
+    public class InventoryViewModel
+    {
+        private IEnumerable<CarViewModel> cars;
+        public InventoryViewModel(IEnumerable<CarViewModel> cars)
+        {
+            this.cars = cars;
+        }
+
+        public IEnumerable<CarViewModel> Cars
+        {
+            get { return this.cars; }
+            set { this.cars = value; }
+        }
+
+    }
+}
