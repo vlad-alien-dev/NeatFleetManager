b4b8df142b9dc3b440638fcf774c72f56871a531
Vladi <vladislav.budnikov@gmail.com>
2020-12-13 22:40:04 +0200

    Adding Autofac and Mapping

---
+++ NeatFleetManagement.Presentation/App_Start/Bootstrapper.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using AutoMapper;
+using NeatFleetManager.Data;
+using NeatFleetManager.Service;
+using NeatFleetManager.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NeatFleetManager.Web.App_Start
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+            SetAutofacContainer();
+        }
+
+        private static void SetAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            // Repositories
+            //builder.RegisterAssemblyTypes(typeof(Repository<Car>).Assembly)
+            //    .Where(t => t.Name.EndsWith("Repository"))
+            //    .AsImplementedInterfaces().InstancePerRequest();
+            // Services
+
+            //builder.RegisterAssemblyTypes(typeof(CarService).Assembly)
+            //   .Where(t => t.Name.EndsWith("Service"))
+            //   .AsImplementedInterfaces().InstancePerRequest();
+
+            builder.RegisterType<Repository<Car>>().As<IRepository<Car>>().InstancePerRequest();
+            builder.RegisterType<CarService>().As<ICarService>().InstancePerRequest();
+
+            var mapper = AutoMapperConfiguration.Configure().CreateMapper();
+            builder.RegisterInstance<IMapper>(mapper);
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ NeatFleetManagement.Presentation/Infrastructure/AutoMapperConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NeatFleetManager.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeatFleetManager.Web.Infrastructure
+{
+    public class AutoMapperConfiguration
+    {
+        public static MapperConfiguration Configure()
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.AddProfile(new PresentationMappingProfile());
+                c.AddProfile(new ServiceMappingProfile());
+            });
+
+            return config;
+        }
+    }
+}
