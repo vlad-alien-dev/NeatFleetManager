3219fe97ea0d1e647d413c99506c9070f453fb62
Vladi <vladislav.budnikov@gmail.com>
2020-12-13 22:23:21 +0200

    Adding utils project

---
+++ NeatFleetManager.Utils/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatFleetManager.Utils
+{
+    public enum CarColor
+    {
+        Red,
+        Yellow,
+        Green
+    }
+    public enum CarCondition
+    {
+        New,
+        Used
+    }
+}
